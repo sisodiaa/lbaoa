f3b4623361388f9864e21bfef6881bed844c957e
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2020-06-24 18:08:23 +0530

    Write system tests for deparment resource

--- test/system/departments_test.rb
+++ test/system/departments_test.rb
@@ -1,45 +1,36 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class DepartmentsTest < ApplicationSystemTestCase
   setup do
-    @department = departments(:one)
+    @department = departments(:horticulture)
   end
 
-  test "visiting the index" do
-    visit departments_url
-    assert_selector "h1", text: "Departments"
+  test 'visiting the index' do
+    visit cms_departments_url
+    assert_selector 'h1', text: 'Departments'
   end
 
-  test "creating a Department" do
-    visit departments_url
-    click_on "New Department"
+  test 'creating a Department' do
+    visit cms_departments_url
+    click_on 'New Department'
 
-    fill_in "Description", with: @department.description
-    fill_in "Title", with: @department.title
-    click_on "Create Department"
+    fill_in 'Description', with: 'Fire and Safety'
+    fill_in 'Title', with: 'Ensuring residents safety from fire'
+    click_on 'Create Department'
 
-    assert_text "Department was successfully created"
-    click_on "Back"
+    assert_text 'Department was successfully created'
+    click_on 'Back'
   end
 
-  test "updating a Department" do
-    visit departments_url
-    click_on "Edit", match: :first
+  test 'updating a Department' do
+    visit cms_departments_url
+    click_on 'Edit', match: :first
 
-    fill_in "Description", with: @department.description
-    fill_in "Title", with: @department.title
-    click_on "Update Department"
+    fill_in 'Description', with: 'New description'
+    click_on 'Update Department'
 
-    assert_text "Department was successfully updated"
-    click_on "Back"
-  end
-
-  test "destroying a Department" do
-    visit departments_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "Department was successfully destroyed"
+    assert_text 'Department was successfully updated'
+    assert_text 'New description'
+    click_on 'Back'
   end
 end

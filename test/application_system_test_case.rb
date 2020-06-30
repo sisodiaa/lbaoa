c605a41c86e485a46b295f304c897dd73d12daf6
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2020-06-30 23:34:01 +0530

    Remove files uploaded during system test runs

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,13 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  def remove_uploaded_files
+    FileUtils.rm_rf("#{Rails.root}/tmp/storage")
+  end
+
+  def after_teardown
+    super
+    remove_uploaded_files
+  end
 end

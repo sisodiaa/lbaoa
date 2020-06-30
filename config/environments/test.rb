46c3e320c865c3ac1aee8f82e74e5c1086fde96e
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2020-06-30 22:17:31 +0530

    Set inline as queue adapter for tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,4 +46,7 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+  # Use inline job processing to make things happen immediately
+  config.active_job.queue_adapter = :inline
 end

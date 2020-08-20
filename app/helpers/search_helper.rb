76043969aa037a44e3d424e30d3dec32b2b044de
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2020-08-20 14:23:39 +0530

    Search posts by category and tag via links
    Category and tags are hyperlinked to URL which show search results

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -4,4 +4,12 @@ module SearchHelper
       [title.titleize, title]
     end.unshift(['All', 'all'])
   end
+
+  def search_post_link(value, attrib, klass = '')
+    link_to(
+      value,
+      search_posts_path(search_post_form: { attrib.to_sym => value }),
+      class: klass
+    )
+  end
 end

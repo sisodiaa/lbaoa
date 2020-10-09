6367369bed81d1d10da8027464266fd9bda6af79
Abhishek Sisodia <mail.abhisheksisodia@gmail.com>
2020-10-09 15:43:40 +0530

    Rails delivery errors in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,13 +53,13 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter     = :sidekiq
-  config.active_job.queue_name_prefix = "lbaoa_production"
+  # config.active_job.queue_name_prefix = "lbaoa_production"
 
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'lbaoa.com' }
   ActionMailer::Base.smtp_settings = {
